e76a19142942748c02536cf762e8626b11ca085d
ayangjibrut <129623348+ayangjibrut@users.noreply.github.com>
2023-09-27 03:13:13 +0700

    Final

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-let colorArray = []
+const colorPicker = document.getElementById('color-picker')
+const colorScheme = document.getElementById('drop-down-menu')
+const getSchemeButton = document.getElementById('get-color-scheme')
 
-
-fetch('https://www.thecolorapi.com/scheme')
-    .then(res => res.json())
-    .then(data => console.log(data))
+getSchemeButton.addEventListener("click", function() {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicker.value.substring(1)}&mode=${colorScheme.value}`)
+        .then(res => res.json())
+        .then(data => {
+            for(let i = 0; i < 5; i++) {
+                document.getElementById(`column${i}`).style.background = data.colors[i].hex.value
+                document.getElementById(`column${i}`).style.cursor = 'pointer'
+                document.getElementById(`row${i}`).textContent = data.colors[i].hex.value
+                document.getElementById(`row${i}`).style.cursor = 'pointer'
+                document.getElementById(`column${i}`).addEventListener("click", function() {
+                    navigator.clipboard.writeText(data.colors[i].hex.value)
+            })
+            
+            document.getElementById(`row${i}`).addEventListener("click", function() {
+                navigator.clipboard.writeText(data.colors[i].hex.value)
+            })
+        }
+    })
+})
